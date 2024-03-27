1fe65eec61f1295c189a749fa231933ad3171bb0
Izdrea Ovidiu <captain.ovidel@gmail.com>
2024-03-27 03:25:55 +0200

    hero section + image, info and images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "www.giallozafferano.it",
+      "flavorthoughts.com",
+      "www.recipetineats.com",
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,87 @@
 import Image from "next/image";
+import pasta from "E:/Projects/basic/landing-page/pasta.jpg";
+import amatriciana from "E:/Projects/basic/landing-page/amatriciana.png";
 
 export default function Home() {
   return (
     <main>
-      <div className="bg-[#1F2937] text-[#f9faf8]">
-        <div className="flex flex-row  w-full h-72">
-          <p className="px-40 text-[24px] text-left">Logo</p>
-          <div className="text-right flex flex-row ml-auto px-40 gap-10">
+      <div className="bg-[#1F2937] text-[#f9faf8] h-96">
+        <div className="flex flex-row ">
+          <p className="px-60 text-[24px] text-left">Logo</p>
+          <div className="text-right flex flex-row ml-auto px-60 gap-10">
             <p className="">header link one</p>
             <p className="">header link two</p>
             <p className="">header link three</p>
           </div>
         </div>
-        <div className="px-40 text-[24px] text-left">
-          <p className="text-[48px] font-extrabold text-[#f9faf8]">
-            Roman Quality!
-          </p>
-          <p>The best Roman pasta in Italy is here at your door!</p>
-          <button className="bg-[#3882f6] rounded-lg">
-            <p className="mx-9 text-[18px]">Order Now!</p>
-          </button>
+        <div className="flex justify-between items-start">
+          <div className="px-60 text-[24px] text-left pt-20">
+            <p className="text-[48px] font-extrabold text-[#f9faf8] ">
+              Roman Quality!
+            </p>
+            <p className="text-[18px]">
+              The best Roman pasta in Italy is here at your door!
+            </p>
+            <button className="bg-[#3882f6] rounded-lg mt-3">
+              <p className="mx-9 my-1 text-[18px]">Order Now!</p>
+            </button>
+          </div>
+          <div className=" pt-20 px-60 ">
+            <Image
+              className="outline outline-2 rounded-lg outline-blue-500 "
+              src={pasta}
+              alt="Pasta"
+              width={500}
+              height={400}
+            />
+          </div>
         </div>
       </div>
-      <div className="h-72"></div>
+      <div className="h-72">
+        <p className="flex justify-center pt-10 text-[36px] font-extrabold text-[#1F2937]">
+          The "Ancient Technique" of making artisanal pasta
+        </p>
+        <div className="flex justify-center pt-10 space-x-24 ">
+          <div className="relative w-44 h-44">
+            <Image
+              className="outline outline-2 rounded-lg outline-blue-500 "
+              src={
+                "https://www.giallozafferano.it/images/ricette/219/21989/foto_hd/hd650x433_wm.jpg"
+              }
+              layout="fill"
+              alt="Cacio"
+            ></Image>
+          </div>
+          <div className="relative w-44 h-44">
+            <Image
+              className="outline outline-2 rounded-lg outline-blue-500"
+              src={
+                "https://flavorthoughts.com/wp-content/uploads/2023/05/Pasta-alla-Gricia-3.jpg"
+              }
+              layout="fill"
+              alt="gricia"
+            ></Image>
+          </div>
+          <div className="relative w-44 h-44">
+            <Image
+              className="outline outline-2 rounded-lg outline-blue-500"
+              src={amatriciana}
+              layout="fill"
+              alt="Amatriciana"
+            ></Image>
+          </div>
+          <div className="relative w-44 h-44">
+            <Image
+              className="outline outline-2 rounded-lg outline-blue-500"
+              src={
+                "https://www.recipetineats.com/wp-content/uploads/2023/01/Carbonara_6a.jpg"
+              }
+              layout="fill"
+              alt="Amatriciana"
+            ></Image>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
